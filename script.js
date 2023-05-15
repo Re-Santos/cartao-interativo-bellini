d5b24773810fb005d12984062c7f0e716b146607
Re-Santos <renata.prog@gmail.com>
2023-05-14 23:36:03 -0300

    :sparkles: feat: Adiciona funções aos botões

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function  btn1(){
+    window.open("https://www.bellinicalcados.com.br/");
+}
+
+function btn2(){
+    window.open("https://www.whatsapp.com/?lang=pt_br")
+}
+
+function btn3(){
+    window.open("https://instagram.com/bellinicalcados?igshid=ZGUzMzM3NWJiOQ==")
+}
+
+function btn4(){
+    window.open("https://www.facebook.com/bellinicalcados?mibextid=ZbWKwL")
+}
